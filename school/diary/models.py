6a34623589820a43acbdb8d342690a47ff4b079f
florantIS <dolnik.nick@gmail.com>
2023-05-13 22:05:40 +0300

    Removed oversighted unneccessary comments

--- school/diary/models.py
+++ school/diary/models.py
@@ -6,15 +6,4 @@ class Student(models.Model):
     language = models.CharField(max_length=15)
     course = models.CharField(max_length=10)
     grade = models.IntegerField()
-
-    # def __init__(self, name = None, language = None):
-    #     self.name = name
-    #     self.language = language
-    #     self.course = ""
-    #     self.grade = None
-
-    # def set_course(self, course):
-    #     self.course = course 
-
-    # def get_grade(self, grade):
-    #     self.grade = grade
+    
