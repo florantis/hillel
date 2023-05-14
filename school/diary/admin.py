04036996e1ec74c81302e6f886b9f79dd7b1c114
florantIS <dolnik.nick@gmail.com>
2023-05-14 22:15:11 +0300

    Completed Homework 4

--- school/diary/admin.py
+++ school/diary/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Student, WeekDay
 # Register your models here.
+
+class WeekDayAdmin(admin.ModelAdmin):
+    list_display = ("title", "note")
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("name", "language", "course")
+
+admin.site.register(WeekDay, WeekDayAdmin)
+admin.site.register(Student, StudentAdmin)
