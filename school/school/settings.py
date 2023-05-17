508b44c649fb9179f5537cf8b0a1d15c340be419
florantIS <dolnik.nick@gmail.com>
2023-05-17 12:20:14 +0300

    Completed the Homework 6 and enhanced experience Did everything that was written in the Homework and also decided to modify my old code, so that the site is much more pleasent to use.
    *Added Headers with simple navigation for diary pages and the index;
    *Made specific page for a login error page.

--- school/school/settings.py
+++ school/school/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'school.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['diary/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
