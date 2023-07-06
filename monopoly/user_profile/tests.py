8252e8cf2192f86c9ce19b100218d0b0d258bfaf
Mykola <dolnik.nick@gmail.com>
2023-07-06 22:04:19 +0300

    Added tests for the new functionality

--- monopoly/user_profile/tests.py
+++ monopoly/user_profile/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 import pytest
 from pytest_django.asserts import assertQuerySetEqual
 from django.contrib.auth.models import User as AuthUser
+from django.contrib.auth import authenticate
 
 from .models import Commentary, Profile
 
@@ -50,3 +51,30 @@ def test_posting_comments_on_user_page(client):
 
     assertQuerySetEqual(response_three_comments.context["comment_list"], db_three_comments, ordered=False)
     assertQuerySetEqual(response_one_comment.context["comment_list"], db_one_comment, ordered=False)
+
+
+@pytest.mark.django_db
+def test_registering_new_user(client):
+    client.post(reverse("register"), {"username": "User1", "password": "securepassword1"})
+    valid_user = AuthUser.objects.get(username="User1")
+    assert valid_user
+    assert Profile.objects.get(auth_user=valid_user)
+
+    client.post(reverse("register"), {"username": "User2", "password": "insecurepassword"})
+    try:
+        invalid_user = AuthUser.objects.get(username="User2")
+    except:
+        invalid_user = None
+    try:
+        invalid_profile = Profile.objects.get(auth_user=invalid_user)
+    except:
+        invalid_profile = None
+    assert not invalid_user
+    assert not invalid_profile
+
+
+@pytest.mark.django_db
+def test_logging_in_user(client):
+    response = client.post(reverse("register"), {"username": "User1", "password": "securepassword1"})
+    valid_user = AuthUser.objects.get(username="User1")
+    assert (response.context["user"] == valid_user)
